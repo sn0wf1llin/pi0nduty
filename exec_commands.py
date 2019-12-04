8795859194abd9201c85906b0fc51cfd33e6f4ee
sn0wf1llin <sn0wf1llin@gmail.com>
2019-12-04 15:13:42 +0300

    interpreter added;

--- exec_commands.py
+++ exec_commands.py
@@ -1,19 +1,30 @@
 import re
+import os
 
 
 COMMANDS_FILE = "commands.txt"
 INTERPRETER_PATTERN = r'^\[\w+\]$'
+interpreters_names_path = {
+    'shell': '/bin/bash -c',
+    'perl': '/usr/bin/perl -e'
+
+}
 
 interpreters_commands = {}
 interpreter = None
 
 with open(COMMANDS_FILE, 'r') as f:
     for i, l in enumerate(f.readlines()):
-        if interpreter is not None and interpreter in interpreters_commands.keys():
-            interpreters_commands[interpreter].append(l.rstrip())
-
         if re.match(INTERPRETER_PATTERN, l):
             interpreter = l[1:-2]
             interpreters_commands[interpreter] = []
-for k, v in interpreters_commands.items():
-    print("{}:{}".format(k, v))
+            continue
+
+        if interpreter is not None and interpreter in interpreters_commands.keys():
+            interpreters_commands[interpreter].append(l.rstrip())
+
+for interp in interpreters_commands.keys():
+    commands = interpreters_commands[interp]
+    for command in commands:
+        os.system("{} \"{}\" >> results.txt ".format(
+            interpreters_names_path[interp], command))
